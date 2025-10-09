91db9af1a45bae70d2c6f247745aa5f83624c888
Achal <achalyturkar@gmail.com>
2025-10-09 11:47:38 +0530

    Active profile

--- Frontend/src/components/Common/ProtectedRoute/ProtectedRoute.jsx
+++ Frontend/src/components/Common/ProtectedRoute/ProtectedRoute.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import Cookies from "js-cookie";
+
 import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute = ({ element: Component }) => {
